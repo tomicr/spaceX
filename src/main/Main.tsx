c15f6a2364876b901bf0887b853844ad6db6acc6
tomicr <93726846+tomicr@users.noreply.github.com>
2021-11-24 17:17:01 +0100

    Infinite scroll react component was added

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Launch from './component/Launch';
-import AllLaunches from './service/AllLaunches';
+import useAllLaunches from './service/useAllLaunches';
 
 const Main = function Main() {
-  const { data: launches } = AllLaunches();
+  const { data: launches } = useAllLaunches();
 
   return (
-    <ul className="main">
-      {launches &&
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        launches.map((launch: any) => (
-          <Launch key={launch.mission_name} launch={launch} />
-        ))}
-    </ul>
+    <div>
+      {launches && (
+        <InfiniteScroll
+          dataLength={launches.length}
+          next={() => useAllLaunches}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <ul className="main">
+            {launches.map((launch: any) => (
+              <Launch key={launch.mission_name} launch={launch} />
+            ))}
+          </ul>
+        </InfiniteScroll>
+      )}
+    </div>
   );
 };
 export default Main;
--- src/main/service/useAllLaunches.tsx
+++ src/main/service/useAllLaunches.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-export default function AllLaunches() {
+function useAllLaunches() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const baseUrl: any = `${process.env.REACT_APP_BASE_URL}launches` as any;
   return useQuery('launches', () => axios.get(baseUrl).then((res) => res.data));
 }
+
+export default useAllLaunches;
