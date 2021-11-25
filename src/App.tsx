d35f46e3363622590f815e507359b0c837ef0aa3
tomicr <93726846+tomicr@users.noreply.github.com>
2021-11-25 16:55:57 +0100

    Implement header for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './scss/App.scss';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Main from './main/Main';
+import Header from './main/component/Header';
 
 const App = function App() {
   const queryClient = new QueryClient();
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Main />
       </QueryClientProvider>
     </div>
